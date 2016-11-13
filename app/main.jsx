048168a67b0296d6423bc9a49cd9746583f851a2
blaze.i.burgess <blaze.i.burgess@gmail.com>
2016-11-13 12:12:18 -0500

    Debuged gulp task error, fixed syntax in StoriesList return placeholder

---
+++ app/main.jsx
@@ -0,0 +1,11 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var StoryList = require("./components/StoryList.jsx");
+
+var _stories = [{name: "First story", tagline: "Moving and of timeless significance"},
+  {name: "Second story", tagline: "Derivative. Rehashing of previous work. Lacks vitality"}]
+
+function render() {
+  ReactDOM.render(<StoryList stories={_stories} />, document.getElementById("container"));
+}
+render();
