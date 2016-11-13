9e941f763afd9472d5749f189e8748e58884cfd6
blaze.i.burgess <blaze.i.burgess@gmail.com>
2016-11-13 11:55:40 -0500

    Add basic story/story-list components, used gulp to integrate bootstrap

---
+++ app/components/StoryList.jsx
@@ -0,0 +1,13 @@
+var React = require("react");
+var StoryInfo = require("./StoryInfo.jsx");
+
+module.exports = React.createClass({
+  render: function () {
+    return(
+      <div>
+	// addStory functionality will go here
+	{this.props.stories.map(function (s, index) {return() }) }
+      </div>
+    )
+  }
+});
