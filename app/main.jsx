4f645e0d8a5ab059ca83639d6065f40b3457f675
blaze.i.burgess <blaze.i.burgess@gmail.com>
2016-11-14 12:10:31 -0500

    Add basic store for stories, gulped, and added store to main.jsx

--- app/main.jsx
+++ app/main.jsx
@@ -1,11 +1,15 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var StoryList = require("./components/StoryList.jsx");
+var storiesStore  = require("./stores/storiesStore");
 
-var _stories = [
-  {name: "First story", tagline: "Moving and of timeless significance"},
-  {name: "Second story", tagline: "Derivative. Rehashing of previous work. Lacks vitality"}
-]
+
+var _stories = storiesStore.getStories();
+
+storiesStore.onChange(function (stories) {
+  _stories = stories;
+  render();
+});
 
 function render() {
   ReactDOM.render(<StoryList stories={_stories} />, document.getElementById("container"));
---
+++ app/stores/storiesStore.js
@@ -0,0 +1,70 @@
+var dispatcher = require("../dispatcher");
+
+function StoryStore () {
+  var listeners = [];
+  var stories = [
+    {
+      name: "First story",
+      tagline: "Original. Masterpiece. Stunning brilliance"
+    },
+    {
+      name: "Second story",
+      tagline: "Garbage. Derivative. Toff"
+    }
+  ];
+
+  function getStories() {
+    return stories;
+  }
+
+  function onChange(listener) {
+    listeners.push(listener);
+  }
+
+  function addStory(story) {
+    stories.push(story);
+    triggerListeners();
+  }
+
+  function deleteStory(story) {
+    var _index;
+    stories.map(function (s, index) {
+      if (s.name === story.name) {
+	_index = index;
+      }
+    });
+    stories.splice(_index, 1);
+    triggerListeners();
+  }
+
+  function triggerListeners() {
+    listeners.forEach(function (listener) {
+      listener(schools);
+    });
+  }
+
+  dispatcher.register(function (payload) {
+    var split = payload.typle.split(":");
+    if (split[0] === "story") {
+      switch (split[1]) {
+        case 'addStory':
+          addStory(payload.story);
+          break;
+        case 'deleteStory':
+          deleteStory(payload.story);
+          break;
+        
+        default:
+          console.log("ERROR: unknown command");
+	  break;
+      }
+    }
+  });
+
+  return {
+    getStories: getStories,
+    onChange: onChange
+  }
+}
+
+module.exports = StoryStore();
